f160800559026efef6af198b134d5f7e16e4a927
adib-santos <adib.santos@code.berlin>
2022-03-23 13:30:45 +0100

    First fixture for tests

--- app/fotos/routes.py
+++ app/fotos/routes.py
@@ -3,8 +3,8 @@ from flask import Blueprint, render_template
 blueprint = Blueprint('fotos', __name__)
 
 fotos_data = {
-    "rome" : {"name": " Old man in rome ", "price": "$50"}, 
-    "quito" : {"name": "Hija con su hija ", "price": "$45"}
+    "rome" : {"name": " Old man in rome ", "price": "$50", "image": "/static/images/selfie.jpg"}, 
+    "quito" : {"name": "Hija con su hija ", "price": "$45", "image": "/static/images/reflection-rome.jpg"}
 }
 
 @blueprint.route('/shop')
