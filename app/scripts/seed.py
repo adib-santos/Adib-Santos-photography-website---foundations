13fb50e6239493e4c9e45b88344b05f18710a42d
adib-santos <adib.santos@code.berlin>
2022-04-04 20:09:41 +0200

    Added photos in specific folders

--- app/scripts/seed.py
+++ app/scripts/seed.py
@@ -6,16 +6,17 @@ app = create_app()
 app.app_context().push()
 
 fotos_data = {
-    "rome" : {"name": " Old man in rome ", "price": 50}, 
-    "quito" : {"name": "Hija con su hija ", "price": 45}
+    "Valencia" : {"name": "massi ", "price": 30},
 }
 
+print("Adding")
+
 for slug, foto in fotos_data.items(): 
-    new_foto = Foto(slug = slug, name = foto['name'], price = foto['price'], slug = foto[slug])
+    new_foto = Foto(slug = slug, name = foto['name'], price = foto['price'])
     db.session.add(new_foto)
+    print(new_foto)
 
 db.session.commit()
-
 
 """
      GOTTA FIND OUT HOW TO FIX THE SHOP TACHADA Y QUE LLEVE A LOS LINKS DINAMICOS 
--- app/shop_outer/routes.py
+++ app/shop_outer/routes.py
@@ -6,13 +6,13 @@ blueprint = Blueprint('fotos', __name__)
 @blueprint.route('/shop')
 def shop():
     all_fotos = Foto.query.all() 
+    print(all_fotos)
     return render_template('shop/index.html', fotos = all_fotos)
 
 @blueprint.route('/shop/<slug>', methods = ['GET']) # aqui borre el  -> methods = ['GET']
 def ind(slug): 
 
     all_fotos = Foto.query.all(id) # Read the querry one part to solve this
-    print(id)
-    print(all_fotos)
-
+    
+    
     return render_template('/shop/ind.html', fotos = all_fotos[slug])
