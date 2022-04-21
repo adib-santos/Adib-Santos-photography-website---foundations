2407e46a3994c5df38208ab2e67fcf8279df3c4a
adib-santos <adib.santos@code.berlin>
2022-04-21 16:10:17 +0200

    reordered db

--- app/models/orders.py
+++ app/models/orders.py
@@ -11,7 +11,6 @@ class Order(db.Model, CRUDMixin):
 
 class Foto(db.Model, CRUDMixin): 
   id = db.Column(db.Integer, primary_key = True)
-  slug = db.Column(db.String(80), unique = True)
   name = db.Column(db.String(80), unique = True)
   price = db.Column(db.Numeric(10, 2))
   picture_url = db.Column(db.String(80))
