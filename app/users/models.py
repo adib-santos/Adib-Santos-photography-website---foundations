0580832d8adc2a04936ac644dd70ea4adf0bff43
adib-santos <adib.santos@code.berlin>
2022-04-21 22:15:56 +0200

    finished registration

--- app/users/models.py
+++ app/users/models.py
@@ -2,5 +2,5 @@ from app.extensions.database import CRUDMixin, db
 
 class User(db.Model, CRUDMixin): 
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(80), index = True, unique = True)
+    email = db.Column(db.String(80), index = True, primary_key = True)
     password = db.Column(db.String(120))
